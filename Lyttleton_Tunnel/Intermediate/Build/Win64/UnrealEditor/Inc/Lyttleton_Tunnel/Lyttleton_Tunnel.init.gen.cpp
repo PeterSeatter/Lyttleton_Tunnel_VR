3ad56934cb4f5d31feff3b71a4393f10867d0f7f
PeterSeatter <peterseatter@gmail.com>
2022-07-05 14:38:19 +1200

    Remaster
    Done some work and got the first bit of C++ in

--- Lyttleton_Tunnel/Intermediate/Build/Win64/UnrealEditor/Inc/Lyttleton_Tunnel/Lyttleton_Tunnel.init.gen.cpp
+++ Lyttleton_Tunnel/Intermediate/Build/Win64/UnrealEditor/Inc/Lyttleton_Tunnel/Lyttleton_Tunnel.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeLyttleton_Tunnel_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x8748D414,
-				0x97CAF4D3,
+				0x44FC24E8,
+				0xCBC27608,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Lyttleton_Tunnel.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_Lyttleton_Tunnel.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Lyttleton_Tunnel(Z_Construct_UPackage__Script_Lyttleton_Tunnel, TEXT("/Script/Lyttleton_Tunnel"), Z_Registration_Info_UPackage__Script_Lyttleton_Tunnel, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x8748D414, 0x97CAF4D3));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Lyttleton_Tunnel(Z_Construct_UPackage__Script_Lyttleton_Tunnel, TEXT("/Script/Lyttleton_Tunnel"), Z_Registration_Info_UPackage__Script_Lyttleton_Tunnel, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x44FC24E8, 0xCBC27608));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
